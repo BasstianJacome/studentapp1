daf1d2266fff25e6dc30405a6691eab4e324bf8e
Vinicio Jacome <vjacomeg@my.centennialcollege.ca>
2024-04-03 12:17:34 -0400

    Start Fresh

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const Dotenv = require('dotenv-webpack');
 const deps = require("./package.json").dependencies;
 module.exports = (_, argv) => ({
   output: {
-    publicPath: "https://studentapp1basstian.netlify.app",
+    publicPath: "http://localhost:3000/",
   },
 
   resolve: {
