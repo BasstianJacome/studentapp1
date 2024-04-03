ae16a4c93f3c6c1cf784dce18e5eebfab0bfdb27
Vinicio Jacome <vjacomeg@my.centennialcollege.ca>
2024-04-03 10:39:38 -0400

    second attempt

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const Dotenv = require('dotenv-webpack');
 const deps = require("./package.json").dependencies;
 module.exports = (_, argv) => ({
   output: {
-    publicPath: "https://timely-hummingbird-586880.netlify.app",
+    publicPath: "https://studentapp1basstian.netlify.app",
   },
 
   resolve: {
